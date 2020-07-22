e2ed4c5314af8f16fb6aa15211bfd4af065e69a6
SRewo <maoam97@gmail.com>
2020-07-22 14:26:18 +0200

    Add Date provider class and interface

---
+++ WeatherStation.Library/DateProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using WeatherStation.Library.Interfaces;
+
+namespace WeatherStation.Library
+{
+    public class DateProvider : IDateProvider
+    {
+        public DateTime GetActualDateTime() => DateTime.Now;
+    }
+}
---
+++ WeatherStation.Library/Interfaces/IDateProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WeatherStation.Library.Interfaces
+{
+    public interface IDateProvider
+    {
+        DateTime GetActualDateTime();
+    }
+}
