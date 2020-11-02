05c4c7be4f5326ab8f7a37d3b8885da9426553f8
SRewo <maoam97@gmail.com>
2020-11-02 12:03:37 +0100

    WS-1 Add basic settings view and viewmodel + settings menu item

---
+++ WeatherStation.App/WeatherStation.App/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace WeatherStation.App.ViewModels
+{
+    public class SettingsViewModel : BindableBase
+    {
+        public SettingsViewModel()
+        {
+        }
+    }
+}
