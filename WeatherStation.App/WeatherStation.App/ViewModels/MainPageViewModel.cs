9377c0229a563ac33eea3ac3f5bcb0f9e005ef43
SRewo <maoam97@gmail.com>
2020-11-18 08:49:14 +0100

    WS-3 Add daily max temp chart to main view

--- WeatherStation.App/WeatherStation.App/ViewModels/MainPageViewModel.cs
+++ WeatherStation.App/WeatherStation.App/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using Intents;
+using Microcharts;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using SkiaSharp;
 using WeatherStation.Library;
 using WeatherStation.Library.Interfaces;
 using Xamarin.Essentials.Interfaces;
@@ -23,6 +27,8 @@ namespace WeatherStation.App.ViewModels
         private WeatherData _weatherData;
 
         private IEnumerable<WeatherData> _weatherHourlyData;
+
+        private Chart _chart;
 
         public MainPageViewModel(IDateProvider dateProvider, IPreferences preferences)
         {
@@ -54,6 +60,12 @@ namespace WeatherStation.App.ViewModels
             set => SetProperty(ref _weatherDailyData, value);
         }
 
+        public Chart Chart
+        {
+            get => _chart;
+            set => SetProperty(ref _chart, value);
+        }
+
         private string _cityName;
 
         public string CityName
@@ -76,6 +88,7 @@ namespace WeatherStation.App.ViewModels
             await GetVariablesFromParameters(parameters);
             await CheckIfRepositoryContainsDailyAndHourlyForecasts();
             await GetData();
+            await CreateChart();
         }
 
         private Task GetVariablesFromParameters(INavigationParameters parameters)
@@ -102,7 +115,7 @@ namespace WeatherStation.App.ViewModels
                 await DownloadWeatherDataFromRepository();
         }
 
-        public Task CheckIfRepositoryContainsDailyAndHourlyForecasts()
+        private Task CheckIfRepositoryContainsDailyAndHourlyForecasts()
         {
             ContainsDailyForecasts = _repositoryStore.DailyForecastsRepository != null;
             ContainsHourlyForecasts = _repositoryStore.HourlyForecastsRepository != null;
@@ -113,11 +126,74 @@ namespace WeatherStation.App.ViewModels
         {
             var currentWeather = await _repositoryStore.CurrentWeatherRepository.GetWeatherDataFromRepository();
             WeatherData = currentWeather.First();
+            WeatherDailyData = ContainsDailyForecasts
+                ? await _repositoryStore.DailyForecastsRepository.GetWeatherDataFromRepository() : null;
         }
 
         private bool IsWeatherDataCurrent()
         {
             return WeatherData != null && DateProvider.GetActualDateTime() - WeatherData.Date <= TimeSpan.FromHours(1);
+        }
+
+        private async Task CreateChart()
+        {
+            if (ContainsDailyForecasts)
+                Chart = await CreateLinearChart(CreateDailyWeatherDataTemperatureChartEntries());
+        }
+
+        private Task<LineChart> CreateLinearChart(IEnumerable<ChartEntry> entries)
+        {
+            var chart = new LineChart
+            {
+                Entries = entries,
+                LineMode = LineMode.Straight,
+                ValueLabelOrientation = Orientation.Horizontal,
+                LineAreaAlpha = 0,
+                BackgroundColor = SKColor.Empty,
+                LabelOrientation = Orientation.Horizontal,
+                LabelTextSize = 25,
+                LineSize = 10,
+                PointMode = PointMode.Circle,
+                PointSize = 18
+            };
+            return Task.FromResult(chart);
+        }
+
+        private IEnumerable<ChartEntry> CreateDailyWeatherDataTemperatureChartEntries()
+        {
+            var data = new List<ChartEntry>();
+            foreach (var weatherData in WeatherDailyData)
+                data.Add(WeatherDailyDataTemperatureToChartEntry(weatherData));
+            return data;
+        }
+
+        private ChartEntry WeatherDailyDataTemperatureToChartEntry(WeatherData data)
+        {
+            var entry = new ChartEntry(data.TemperatureMax.Value)
+            {
+                Label = data.Date.ToShortDateString(),
+                ValueLabel = data.TemperatureMax.ToString(),
+                Color = GetProperSKColorAccordingToTemperature(data.TemperatureMax)
+            };
+            return entry;
+        }
+
+        private SKColor GetProperSKColorAccordingToTemperature(Temperature temperature)
+        {
+            var tempValue = temperature.Value;
+            if(tempValue > 35)
+                return SKColor.Parse("#ffee58");
+            if(tempValue > 30)
+                return SKColor.Parse("#f9a825");
+            if(tempValue > 25)
+                return SKColor.Parse("#ef6c00");
+            if(tempValue > 5)
+                return SKColor.Parse("#9e9e9e");
+            if(tempValue > 0)
+                return SKColor.Parse("#1a237e");
+            if(tempValue > -5)
+                return SKColor.Parse("#1e88e5");
+            return SKColor.Parse("#4dd0e1");
         }
     }
 }
