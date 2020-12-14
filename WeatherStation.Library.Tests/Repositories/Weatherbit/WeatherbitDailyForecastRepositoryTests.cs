0bba79155545fc8231f0fc55a4662606f305310c
SRewo <maoam97@gmail.com>
2020-12-14 09:27:51 +0100

    WS-34 Add weatherbitApi daily weather forecasts

---
+++ WeatherStation.Library.Tests/Repositories/Weatherbit/WeatherbitDailyForecastRepositoryTests.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using RestSharp;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherStation.Library.Repositories.Weatherbit;
+using Xunit;
+
+namespace WeatherStation.Library.Tests.Repositories.Weatherbit
+{
+    public class WeatherbitDailyForecastRepositoryTests
+    {
+        private string _testResponseLocation = "Repositories/Weatherbit/TestResponses/dailyWeather.json";
+
+        [Fact]
+        public async Task GetWeatherDataFromRepository_ValidExecution_ReturnsNotEmptyList()
+        {
+            var classes = await CreateTestClasses();
+
+            var items = await classes.Item1.GetWeatherDataFromRepository();
+
+            Assert.NotEmpty(items);
+        }
+
+        private async Task<(WeatherbitDailyForecastRepository, Mock<IRestClient>)> CreateTestClasses()
+        {
+            var client = await CommonMethods.CreateRestClientMock(_testResponseLocation);
+            var repository = new WeatherbitDailyForecastRepository(client.Object,"", (0,0));
+            repository.Language = "pl-PL";
+            return (repository, client);
+        }
+
+        [Fact]
+        public async Task GetWeatherDataFromRepository_ValidExecution_ReturnsForecastFor16Days()
+        {
+            var classes = await CreateTestClasses();
+
+            var items = await classes.Item1.GetWeatherDataFromRepository();
+
+            Assert.Equal(16, items.Count());
+        }
+
+        [Fact]
+        public async Task GetWeatherDataFromRepository_ValidExecution_DoesNotContaionDefaultWeatherDataObjects()
+        {
+            var classes = await CreateTestClasses();
+
+            var items = await classes.Item1.GetWeatherDataFromRepository();
+
+            Assert.DoesNotContain(new WeatherData(), items);
+        }
+    }
+}
---
+++ WeatherStation.Library/Repositories/Weatherbit/WeatherbitDailyForecastRepository.cs
@@ -0,0 +1,45 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WeatherStation.Library.Repositories.Weatherbit
+{
+    public class WeatherbitDailyForecastRepository : WeatherRestRepository
+    {
+        private (double, double) _coordinates;
+        public WeatherbitDailyForecastRepository(IRestClient restClient, string apiKey, (double,double) coordinates) : base(restClient, "forecast/daily", apiKey)
+        {
+            _coordinates = coordinates;
+        }
+
+        protected override Task AddParametersToRequest(IRestRequest request)
+        {
+            request.AddParameter("key", ApiKey);
+            request.AddParameter("lang", Language.Remove(2));
+            request.AddParameter("lat",_coordinates.Item1);
+            request.AddParameter("lon", _coordinates.Item2);
+            return Task.FromResult(request);
+        }
+
+        protected override IEnumerable<WeatherData> CreateWeatherDataListFromResult(dynamic dynamicResult)
+        {
+            var d = (object) dynamicResult.data;
+            return base.CreateWeatherDataListFromResult(d);
+        }
+
+        protected override WeatherData BuildWeatherDataFromDynamicObject(dynamic dynamicObject)
+        {
+            var builder = new WeatherDataBuilder();
+            builder.SetMinTemperature((float) dynamicObject.min_temp, TemperatureScale.Celsius)
+                .SetMaxTemperature((float) dynamicObject.max_temp, TemperatureScale.Celsius)
+                .SetWeatherDescription((string) dynamicObject.weather.description)
+                .SetChanceOfRain((int) dynamicObject.pop)
+                .SetWindSpeed((int) dynamicObject.wind_spd)
+                .SetPressure((int) dynamicObject.pres)
+                .SetDate(DateTime.Parse((string) dynamicObject.datetime));
+            return builder.Build();
+
+        }
+    }
+}
