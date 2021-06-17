4ec2c69c35c7cd5f0c8ef3b83701dfe1dd709364
SRewo <maoam97@gmail.com>
2021-06-17 20:37:09 +0200

    WS-58 Add file for app api keys

--- WeatherStation.App/WeatherStation.App/AppApiKeys.cs
+++ WeatherStation.App/WeatherStation.App/AppApiKeys.cs
@@ -4,6 +4,5 @@
     {
         public static readonly string AccuWeatherApiKey = "";
         public static readonly string PositionstackApiKey = "";
-        public static readonly string WeatherbitApiKey = "";
     }
 }
